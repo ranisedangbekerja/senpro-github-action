c3b8131675251a000f38535004697ef8dccdb66c
ranisedangbekerja <raninirmalaprakoso@mail.ugm.ac.id>
2025-02-21 14:46:32 +0700

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Nama] | [NIU]
+            [Rani Nirmala Prakoso] | [493982]
           </h5>
         </div>
       </main>
